868b95c15cf8eb26dc500158572f60c7e8f6f877
root <root@vin.(none)>
2012-04-06 14:06:37 +0530

    Added threshold functionality for sepcifying colors for different thresholds

--- gauge.js
+++ gauge.js
@@ -21,10 +21,18 @@ var arc = function(hPos, vPos, size, startingPoint, endingPoint) {
  * percent   = percentage you want rendered
  * fill      = background fill color
  * highlight = highlight color used for middle circle
+ * opts	     = additonal parameters	
  */
 
-var gauge = function(div, percent, fill, highlight) {
+var gauge = function(div, percent, fill, highlight, opts) {
   this.canvas = Raphael(div, 300, 100);
+ 
+  if(opts !== undefined ){
+    if(opts.threshold !== undefined){
+    opts.threshold['0']=fill;
+    fill = thresholdCalculator(opts.threshold,percent,fill); 
+    }
+  }
 
   this.background         = this.canvas.path(arc(80, 80, 76, -180, 0)).attr({ "fill": "#f2f2f2", "stroke": "none" });
   this.fill               = this.canvas.path(arc(80, 80, 76, -180, -180 * (1 + (percent / 100)))).attr({ "fill": fill, "stroke": "none" });
@@ -34,13 +42,42 @@ var gauge = function(div, percent, fill, highlight) {
 
   return {
     fill:this.fill,
+    originalFill:this.originalFill,
+    opts:this.opts,
     indicator:this.indicator,
     canvas:this.canvas,
     update:function(percent) {
       this.indicator.rotate(-this.indicator.rotation, 80, 80).rotate(180 * (1 + (percent / 100)), 80, 80);
       this.indicator.rotation = 180 * (1 + (percent / 100));
+      
+      if(opts !== undefined ){
+        if(opts.threshold !== undefined){
+          fill = thresholdCalculator(opts.threshold,percent);
+	 }
+       } 
 
       this.fill.animate({ path: arc(80, 80, 76, -180, -180 * (1 + (percent / 100))) }, 300, "elastic").attr({ "fill": fill, "stroke": "none" });
     }
   }
 };
+
+/*
+ * threshold  = object which holds the thresholds and colors to be rendered
+ * percent    = percentage of gauge
+ * fill	      = original fill color
+ */
+
+function thresholdCalculator(threshold,percent){
+  hVal = 0
+  fill = threshold['0']
+  for(var i in threshold)
+  {
+   if(percent >= i){
+     if(hVal <= i){
+     hVal = i;
+     fill = threshold[i];
+     }
+   }
+  }
+ return fill;
+}
